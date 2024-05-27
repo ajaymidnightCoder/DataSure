7215aae15d682fc111c70a82e9913177db5a8cb7
Ajay <ajayace3@gmail.com>
2024-05-27 22:46:51 +0530

    service and contract projects

---
+++ DataSure.Contracts/Models/Enum/DataTypeEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSure.Contracts.Models.Enum
+{
+    public enum DataTypeEnum
+    {
+        Int = 0,
+        String = 1,
+        Date = 2,
+        Email = 3,
+        DateTime = 4
+    }
+}
---
+++ DataSure.Service/AdminService/EntitiyConfigService.cs
@@ -0,0 +1,20 @@
+﻿using DataSure.Contracts.Interface.AdminService;
+using DataSure.Contracts.Models.AdminModel;
+
+namespace DataSure.Service.AdminService
+{
+    public class EntitiyConfigService : IEntitiyConfigService
+    {
+        public List<EntityConfigModel> GetAll()
+        {
+            var list = new List<EntityConfigModel>();
+            return list;
+        }
+
+        public List<PropertyConfigModel> GetPropertyByEntitiy(int entityId)
+        {
+            var list = new List<PropertyConfigModel>();
+            return list;
+        }
+    }
+}
