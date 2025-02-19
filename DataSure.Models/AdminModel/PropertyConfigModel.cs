c761de82b816f0f4cd117bece4b8431a4f27080b
Ajay <ajayace3@gmail.com>
2025-02-19 22:51:04 +0530

    changes to property config model

--- DataSure.Models/AdminModel/PropertyConfigModel.cs
+++ DataSure.Models/AdminModel/PropertyConfigModel.cs
@@ -3,11 +3,18 @@
 
     public class PropertyConfigModel
     {
+        public bool Is { get; set; }
         public string Name { get; set; }
         public string Code { get; set; }
         public DataTypeEnum DataType { get; set; }
         public bool IsRequired { get; set; }
         public int? LengthInChar { get; set; }
+
+        // New properties
+        public bool IsPrimaryKey { get; set; }
+        public bool IsUnique { get; set; }      
+        public bool IsForeignKey { get; set; } 
+        public string? ForeignKeyReference { get; set; } //Table reference for foreign key
     }
 
 }
